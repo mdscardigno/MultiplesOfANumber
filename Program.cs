bc6b76924984b2f496b29b9394229ff6d866d709
Milagro Scardigno <mdscardigno@gmail.com>
2021-11-05 14:12:00 -0400

    while loop and condition to print multiples of 3

--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 
 namespace MultiplesOfANumber
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the multiples of the number 3 from 1 to 1000.");
+            int number = 0;
+            while (number <= 1000)
+            {
+                if (number % 3 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+                number++;
+            }
+            Console.ReadKey();
+        }
     }
-  }
 }
